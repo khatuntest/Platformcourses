cc7ad924dfb8c7dfd2400baf77657d255c83d746
mahamed ali saed <master.doc333@gmail.com>
2024-12-17 20:29:09 +0200

    Add files via upload

--- Project/Learning-Management-System/src/main/java/com/project/Learning/Management/System/Model/Assignment.java
+++ Project/Learning-Management-System/src/main/java/com/project/Learning/Management/System/Model/Assignment.java
@@ -1,5 +1,24 @@
 package com.project.Learning.Management.System.Model;
 
-public class Assignment {
+import com.project.Learning.Management.System.service.NotificationService;
 
+import java.util.UUID;
+
+public class Assignment {
+    private final NotificationService notificationService = new NotificationService();
+
+    public void gradeAssignment(String studentId, String assignmentName, int grade) {
+        // منطق التقييم
+        // ...
+
+        // إرسال إشعار للطالب
+        String message = "The assignment has been graded " + assignmentName + " grade: " + grade;
+        Notification notification = new Notification(
+                UUID.randomUUID().toString(),
+                studentId,
+                message
+        );
+        notificationService.addNotification(notification);
+    }
 }
+
--- Project/Learning-Management-System/src/main/java/com/project/Learning/Management/System/Model/Courses.java
+++ Project/Learning-Management-System/src/main/java/com/project/Learning/Management/System/Model/Courses.java
@@ -1,5 +1,33 @@
 package com.project.Learning.Management.System.Model;
 
+import com.project.Learning.Management.System.service.NotificationService;
+
+import java.util.UUID;
+
 public class Courses {
+    private final NotificationService notificationService = new NotificationService();
     public void enrollInCourse(Courses course ){}
+
+
+
+    public void enrollStudent(String studentId, String courseId, String instructorId) {
+            // منطق تسجيل الطالب في الكورس
+            // ...
+
+            // إرسال إشعار للمدرس
+        String message = "A new student (ID: " + studentId + ") has enrolled in the course: " + courseId;
+        Notification notification = new Notification(
+                    UUID.randomUUID().toString(),
+                    instructorId,
+                    message
+            );
+            notificationService.addNotification(notification);
+        }
+    }
+
+
+
+
+
+
 }
